31a4269929ad3382a81fab580db14f76efd3bb5c
Andrey Pervushin <andrey.pervushin@appus.me>
2015-10-13 15:03:40 +0300

    Version for CocoaControls

---
+++ demo/CircleTimer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CircleTimer
+//
+// Created by Kirill Serebriakov on 9/25/14.
+// Copyright (c) 2014 Appus Studio LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ demo/CircleTimer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CircleTimer
+//
+// Created by Kirill Serebriakov on 9/25/14.
+// Copyright (c) 2014 Appus Studio LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
